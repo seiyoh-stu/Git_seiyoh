2c840c686a24b49b642e67587fbd68255ea678be
seiyoh-stu <138738958+seiyoh-stu@users.noreply.github.com>
2023-07-20 10:13:00 +0900

    a
    a

---
+++ C言語に使う場所｛１/C言語に使う場所｛１/InputControl_.cpp
@@ -0,0 +1,121 @@
+#include "DxLib.h"
+#include "InputControl.h"
+
+/*****************
+*変数宣言
+*****************/
+int old_button;
+int now_button;
+int mouse_position_x;
+int mouse_position_y;
+
+/****************
+*プロトタイプ宣言
+****************/
+
+/****************
+*入力制御機能：初期化処理
+* 引数：なし
+* 戻り値：なし
+****************/
+
+void Input_Initialize(void)
+{
+	old_button = NULL;
+	now_button = NULL;
+	mouse_position_x = NULL;
+	mouse_position_y = NULL;
+}
+
+/****************
+*入力制御機能：更新処理
+* 引数：なし
+* 戻り値：なし
+****************/
+
+void Input_Updata(void)
+{
+	int ret = FALSE;
+	//ESCキーが押されたらループを抜ける
+	if (CheckHitKey(KEY_INPUT_ESCAPE))
+	{
+		ret = TRUE;
+	}
+
+	return = ret;
+}
+
+/****************
+*入力制御機能：入力情報処理(離した瞬間)
+* 引数：指定するマウスのボタン
+* 戻り値：TRUE(入力された),FALSE(未入力）
+****************/
+
+int GetOldKey(int key)
+{
+	int ret = FALSE;
+
+	if ((key & old_button) != FALSE)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+/****************
+*入力制御機能：入力情報処理(推してる)
+* 引数：指定するマウスのボタン
+* 戻り値：TRUE(入力された),FALSE(未入力）
+****************/
+
+int GetNowKey(int key)
+{
+	int ret = FALSE;
+
+
+	if ((key & now_button) != FALSE)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+/****************
+*入力制御機能：入力情報処理(推した瞬間)
+* 引数：指定するマウスのボタン
+* 戻り値：TRUE(入力された),FALSE(未入力）
+****************/
+int GetKeyFlg(int key)
+{
+	int ret = FALSE;
+	intkeyflg = now_botton & ~old_button;
+
+	if ((key & keyflg) != FALSE)
+	{
+		ret=TRUE
+	}
+	return ret;
+}
+
+/****************
+*入力制御機能マウスの座標取得処理
+* 引数：なし
+* 戻り値：マウスカーソルのX座標情報
+****************/
+
+int GetMousePositionX(void)
+{
+	return mouse_position_x;
+}
+
+
+/****************
+*入力制御機能マウスの座標取得処理
+* 引数：なし
+* 戻り値：マウスカーソルのY座標情報
+****************/
+
+int GetMousePositionY(void)
+{
+	return mouse_position_y;
+}
